0a369b7dd89d898e2ffd9e6760348eed303e2a5d
Muhammad Sheraz <muhammadshiraz492@gmail.com>
2026-03-10 03:02:18 +0500

    Finalize the script

--- scraper_modules/get_list_price.py
+++ scraper_modules/get_list_price.py
@@ -1,9 +1,6 @@
 
 def get_list_price(page):
-    """
-    Extracts the list/original price of the product.
-    Returns a string like "$39.99". If not found, returns empty string.
-    """
+
     try:
         list_price = page.locator(".basisPrice .a-offscreen").first.inner_text().strip()
         return list_price
